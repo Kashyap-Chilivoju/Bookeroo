78292824e18ab09b680ed61568fd38724f9b5649
William Hu S3856557 <S3856557@student.rmit.edu.au>
2021-08-31 19:18:32 +1000

    Updating code with setJWTToken

--- FrontEnd/myfirstapp/src/securityUtils/setJWTToken.js
+++ FrontEnd/myfirstapp/src/securityUtils/setJWTToken.js
@@ -1,6 +1,11 @@
+import axios from "axios";
 
-export function setJWTToken (jwtToken) {
-
+const setJWTToken = token => {
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = token;
+    } else {
+        delete axios.defaults.headers.common["Authorization"];
+    }
 };
 
-export default setJWTToken();
+export default setJWTToken; 
