28c825e9f67324ec19c7189356eaccdc05ea0b82
s3544695 <s3544695@student.rmit.edu.au>
2021-09-03 18:08:32 +1000

    Carousel for homepage. Work in Progress

--- FrontEnd/myfirstapp/src/components/Layout/Landing.js
+++ FrontEnd/myfirstapp/src/components/Layout/Landing.js
@@ -8,20 +8,38 @@ class Landing extends Component {
         <div className="light-overlay landing-inner text-dark">
           <div className="container">
             <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">
-                  Home Page
-                </h1>
-                <p className="lead">
-                  Create your account to join
-                </p>
-                <hr />
-                <Link className="btn btn-lg btn-primary mr-2" to="/register">
-                  Sign Up
-                </Link>
-                <Link className="btn btn-lg btn-secondary mr-2" to="/login">
-                  Login
-                </Link>
+              <div className="col-md-12">
+
+                <div id = "bookCarousel" class="carousel slide" data-bs-ride="carousel">
+                  <div class = "carousel-indicators">
+                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="0" class="active" aria-current = "true" aria-label = "Slide 1"></button>
+                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="1" aria-label = "Slide 2"></button>
+                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="2" aria-label = "Slide 3"></button>
+                  </div>
+
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class = "d-block w-100" src="800x400-1.png" alt="First slide"></img>
+                    </div>
+
+                    <div class="carousel-item">
+                      <img class = "d-block w-100" src="800x400-2.png" alt="Second slide"></img>
+                    </div>
+
+                    <div class="carousel-item">
+                      <img class = "d-block w-100" src="800x400-3.png" alt="Third slide"></img>
+                    </div>
+                  </div>
+
+                  <button class = "carousel-control-prev" type="button" data-bs-target="#bookCarousel" data-bs-slide="prev">
+                    <span class = "carousel-control-prev-icon" aria-hidden="true"></span>
+                  </button>
+
+                  <button class = "carousel-control-next" type="button" data-bs-target="#bookCarousel" data-bs-slide="next">
+                    <span class = "carousel-control-next-icon" aria-hidden="true"></span>
+                  </button>
+                </div>
+
               </div>
             </div>
           </div>
