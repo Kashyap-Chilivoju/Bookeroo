70214efb452d3567401ffd76c7222a867565b6fe
s3544695 <s3544695@student.rmit.edu.au>
2021-09-06 22:50:30 +1000

    Updated FrontEnds and added Publish page

---
+++ FrontEnd/myfirstapp/src/components/BuySellAndPublish/Buy.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Buy extends Component {
+    render() {
+        return (
+            <form>
+                <div class="form-group">
+                    <label for="bookTitle">Book Title</label>
+                    <input type="text" class="form-control" id="bookTitle"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="bookYear">Book Year</label>
+                    <input type="text" class="form-control" id="bookYear"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="bookGenre">Book Genre</label>
+                    <input type="text" class="form-control" id="bookGenre"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="isbn">ISBN</label>
+                    <input type="text" class="form-control" id="isbn"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" row="3"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Buy</button>
+
+                
+            </form>
+
+        );
+    }
+}
+
+export default Buy;
--- FrontEnd/myfirstapp/src/components/Layout/Landing.js
+++ FrontEnd/myfirstapp/src/components/Layout/Landing.js
@@ -10,34 +10,36 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12">
 
-                <div id = "bookCarousel" class="carousel slide" data-bs-ride="carousel">
-                  <div class = "carousel-indicators">
-                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="0" class="active" aria-current = "true" aria-label = "Slide 1"></button>
-                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="1" aria-label = "Slide 2"></button>
-                      <button type = "button" data-bs-target="#bookCarousel" data-bs-slide-to="2" aria-label = "Slide 3"></button>
-                  </div>
+                <div id = "bookCarousel" class="carousel slide" data-ride="carousel">
+                  <ol class = "carousel-indicators">
+                      <li type = "button" data-target="#bookCarousel" data-slide-to="0" class="active"></li>
+                      <li type = "button" data-target="#bookCarousel" data-slide-to="1"></li>
+                      <li type = "button" data-target="#bookCarousel" data-slide-to="2"></li>
+                  </ol>
 
                   <div class="carousel-inner">
                     <div class="carousel-item active">
-                      <img class = "d-block w-100" src="800x400-1.png" alt="First slide"></img>
+                      <img class = "d-block w-100" src="800x400-1.png"></img>
                     </div>
 
                     <div class="carousel-item">
-                      <img class = "d-block w-100" src="800x400-2.png" alt="Second slide"></img>
+                      <img class = "d-block w-100" src="800x400-2.png"></img>
                     </div>
 
                     <div class="carousel-item">
-                      <img class = "d-block w-100" src="800x400-3.png" alt="Third slide"></img>
+                      <img class = "d-block w-100" src="800x400-3.png"></img>
                     </div>
                   </div>
 
-                  <button class = "carousel-control-prev" type="button" data-bs-target="#bookCarousel" data-bs-slide="prev">
+                  <a class = "carousel-control-prev" role="button" href="#bookCarousel" data-slide="prev">
                     <span class = "carousel-control-prev-icon" aria-hidden="true"></span>
-                  </button>
+                    <span class="sr-only">Previous</span>
+                  </a>
 
-                  <button class = "carousel-control-next" type="button" data-bs-target="#bookCarousel" data-bs-slide="next">
+                  <a class = "carousel-control-next" role="button" href="#bookCarousel" data-slide="next">
                     <span class = "carousel-control-next-icon" aria-hidden="true"></span>
-                  </button>
+                    <span class="sr-only">Next</span>
+                  </a>
                 </div>
 
               </div>
