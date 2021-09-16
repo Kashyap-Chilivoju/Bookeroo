f99628daedc10af82c5720cf71d6d8665f7ae133
s3544695 <s3544695@student.rmit.edu.au>
2021-09-16 20:40:42 +1000

    Re added changes after login fix

--- FrontEnd/myfirstapp/src/components/Layout/Header.js
+++ FrontEnd/myfirstapp/src/components/Layout/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
- class Header extends Component {
+class Header extends Component {
     render() {
         return (
             <div>
-            <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
             <div className="container">
-                <a className="navbar-brand" href="Dashboard.html">
-                    Person Management Tool
+                <a className="navbar-brand" href="/Dashboard">
+                    Bookeroo
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
                     <span className="navbar-toggler-icon" />
@@ -21,18 +21,26 @@ import React, { Component } from 'react'
                             </a>
                         </li>
                     </ul>
+
+                    <form class = "form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                                <button class = "btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
     
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link " href="register.html">
-                                Sign Up
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="login.html">
-                                Login
-                            </a>
-                        </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/login">
+                            Login
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link " href="/register">
+                            Register
+                        </a>
+                    </li>
+                    <li className ="nav-item">
+                        <a class="fa fa-shopping-cart" href="/cart"></a>
+                    </li>
                     </ul>
                 </div>
             </div>
